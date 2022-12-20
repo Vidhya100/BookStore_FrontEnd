d758a8cafea164bb42d36ad3b5b04bcec4c7538b
Vidhya100 <vidhyadarade1@gmail.com>
2022-12-20 23:21:12 +0530

    token added

--- src/app/components/forget-password/forget-password.component.ts
+++ src/app/components/forget-password/forget-password.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/userServices/user.service';
 
 @Component({
@@ -10,7 +12,8 @@ import { UserService } from 'src/app/services/userServices/user.service';
 export class ForgetPasswordComponent {
   forgotForm! : FormGroup;
   submitted = false;
-  constructor(private form:FormBuilder,private user: UserService){}
+  token:any;
+  constructor(private form:FormBuilder,private user: UserService,private activeRoute: ActivatedRoute,private _snackBar: MatSnackBar){}
 
   ngOnInit(): void {
     this.forgotForm = this.form.group({
@@ -27,10 +30,9 @@ export class ForgetPasswordComponent {
     // stop here if form is invalid
     if (this.forgotForm.valid) {
       let payload = {
-        email: this.forgotForm.value.email,
-        service : "advance" 
+        email: this.forgotForm.value.email
       }
-      //this.token = this.activeRoute.snapshot.paramMap.get('taken');
+      this.token = this.activeRoute.snapshot.paramMap.get('token');
       console.log(payload);
        this.user.forgetPassword(payload).subscribe((response:any)=>{
         console.log(response)
