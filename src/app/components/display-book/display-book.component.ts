970459535de1a562dd2346b602e7f18eefde4d3b
Vidhya Darade <vidhyadarade1@gmail.com>
2022-12-22 18:30:34 +0530

    booklist declair in get all notes

--- src/app/components/display-book/display-book.component.ts
+++ src/app/components/display-book/display-book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BookService } from 'src/app/services/bookService/book.service';
 
 @Component({
@@ -8,7 +8,7 @@ import { BookService } from 'src/app/services/bookService/book.service';
 })
 export class DisplayBookComponent implements OnInit {
 
-  bookList:any;
+  @Input() bookList:any;
   totalbooks:any;
   sortBy:any='Sort by relevance';
 
