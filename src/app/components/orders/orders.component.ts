8a7d99dc15d14eadfdb5382039c240f0ff4bf0e5
Vidhya Darade <vidhyadarade1@gmail.com>
2022-12-30 17:38:58 +0530

    UI done , some data not rendering

---
+++ src/app/components/orders/orders.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/orderService/order.service';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent {
+
+  orderlist:any;
+
+  constructor(private orderser: OrderService) { }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders(){
+    this.orderser.getOrders().subscribe((response: any) => {
+      console.log("Got All Orders", response.data);
+      this.orderlist = response.data;
+    });
+  }
+}
